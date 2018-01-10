5f21b38ad0aaa436423ee197494f4f52090c1ab8
John A. Craft <john.craft@geocent.com>
2018-01-10 09:36:47 -0600

    Initial phase1a checkin

--- Assets/Rotate.cs
+++ Assets/Rotate.cs
@@ -11,6 +11,6 @@ public class Rotate : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Rotate(Vector3.up * Time.deltaTime * 15f);
+        //transform.Rotate(Vector3.up * Time.deltaTime * 15f);
 	}
 }
